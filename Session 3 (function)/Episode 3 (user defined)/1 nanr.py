bed0386db04344680aecaaa205d9b2e525b607bd
Kaviyarasuchand <162870412+Kaviyarasuchand@users.noreply.github.com>
2024-04-15 07:14:56 +0000

    modified 1 nanr.py in Episode 3 (user defined)

--- Session 3 (function)/Episode 3 (user defined)/1 nanr.py
+++ Session 3 (function)/Episode 3 (user defined)/1 nanr.py
@@ -1,6 +1,6 @@
 # Function to print a simple greeting
 def greet():
-    print("Hello! Welcome to Python user-defined functions.")
+    print("Hello! Welcome to Python user-defined functions...")
 
 # Call the function
 greet()
